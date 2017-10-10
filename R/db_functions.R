e3e3aa11b33c620d0103b5ec1ccae47ac2192de9
John Bannister <jbannister@airsci.com>
2017-10-10 09:56:37 -0700

    added display of database calls

--- R/db_functions.R
+++ R/db_functions.R
@@ -2,6 +2,7 @@
 # query of AWS saltonsea database
 query_salton <- function(query){
   a <- Sys.getenv(c("PSQL_HOST_SS", "PSQL_PASSWORD_SS", "PSQL_USER", "PSQL_PORT"))
+    print(a)
   con <- RPostgreSQL::dbConnect("PostgreSQL", host=a[1], port=a[4],
                                 dbname="saltonsea", user=a[3], password=a[2])
   dat <- RPostgreSQL::dbGetQuery(con, query)
