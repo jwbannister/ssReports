288e3b4d1dde427cb2c8d578dc853514b4a5d18f
John Bannister <jbannister@airsci.com>
2018-11-06 11:01:38 -0800

    fixed stringstofactors issue

--- scripts/met_summary.R
+++ scripts/met_summary.R
@@ -14,16 +14,16 @@ met_clean <- met_clean[ , 1:9]
 met_summary <- vector(mode="list", length=length(areas))
 names(met_summary) <- areas
 for (i in areas){
-temp <- met_clean %>% filter(deployment==i) %>%
-    select(-deployment) %>%
-    gather(measure, value, -datetime) %>%
-    group_by(measure) %>%
-    summarize(data.capture=sum(!is.na(value))/data_hours,
-              monthly.mean=mean(value, na.rm=T), 
-              max.hour=ifelse(sum(!is.na(value))>0, 
-                              max(value, na.rm=T), NA), 
-              min.hour=ifelse(sum(!is.na(value))>0, 
-                              min(value, na.rm=T), NA))
+    temp <- met_clean %>% filter(deployment==i) %>%
+        select(-deployment) %>%
+        gather(measure, value, -datetime) %>%
+        group_by(measure) %>%
+        summarize(data.capture=sum(!is.na(value))/data_hours,
+                  monthly.mean=mean(value, na.rm=T), 
+                  max.hour=ifelse(sum(!is.na(value))>0, 
+                                  max(value, na.rm=T), NA), 
+                  min.hour=ifelse(sum(!is.na(value))>0, 
+                                  min(value, na.rm=T), NA))
     temp[ , 2] <- sapply(temp[ , 2], 
                          function (x) paste0(format(round(x, 2)*100, nsmall=0), "%"))
     for (j in seq(1,7)){
@@ -31,11 +31,12 @@ temp <- met_clean %>% filter(deployment==i) %>%
         temp[j, 3:5] <- sapply(temp[j, 3:5], 
                        function (x) format(round(as.numeric(x), round_num), nsmall=1))
     }
-temp[temp=='NaN' | temp =='NA'] <- "-"
-met_summary[[i]] <- as.data.frame(t(temp), optional=T)[-1, ]
-colnames(met_summary[[i]]) <- temp$measure
-met_summary[[i]] <- met_summary[[i]] %>%
-    select(ws, wd, at_2m, rh_2m, delta_temp_2m, delta_temp_10m, solar_rad)
-met_summary[[i]]$wd[2:4] <- "-"
+    temp[temp=='NaN' | temp =='NA'] <- "-"
+    met_summary[[i]] <- as.data.frame(t(temp), optional=T,
+                                      stringsAsFactors=F)[-1, ]
+    colnames(met_summary[[i]]) <- temp$measure
+    met_summary[[i]] <- met_summary[[i]] %>%
+        select(ws, wd, at_2m, rh_2m, delta_temp_2m, delta_temp_10m, solar_rad)
+    met_summary[[i]]$wd[2:4] <- "-"
 }
 met_summary <- met_summary[sort(names(met_summary))]
